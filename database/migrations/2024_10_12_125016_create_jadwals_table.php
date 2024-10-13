3159c57857ee61f56642b62b47cea9b2b230d368
mixiii0006 <natasyasalsabilla0905@gmail.com>
2024-10-13 21:05:51 +0700

    rangka

--- database/migrations/2024_10_12_125016_create_jadwals_table.php
+++ database/migrations/2024_10_12_125016_create_jadwals_table.php
@@ -6,14 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
         Schema::create('jadwals', function (Blueprint $table) {
             $table->id();
-            
+
             $table->timestamps();
         });
     }
