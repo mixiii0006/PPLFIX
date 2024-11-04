61b23c350f708ef5f92f04b3f74643789714ae7a
mixiii0006 <natasyasalsabilla0905@gmail.com>
2024-11-04 19:09:24 +0700

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -177,17 +177,7 @@
                       @endif
                       @endauth
 
-                      {{-- @auth
-                      @if(Auth::user()->role=='user')
-                      <li>
-                        <a
-                          href="#"
-                          class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                          >Request Ruangan</a
-                        >
-                      </li>
-                      @endif
-                      @endauth --}}
+                      
                     </ul>
                   </li>
                   <li>
