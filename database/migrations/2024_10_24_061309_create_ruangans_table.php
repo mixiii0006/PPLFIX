2b0de64cf3dd965b4c1a8bc05b93d7525e34d9ca
AnjasFedo <fedoaf98@gmail.com>
2024-11-20 15:46:29 +0700

    feat: ruangan tipe

--- database/migrations/2024_10_24_061309_create_ruangans_table.php
+++ database/migrations/2024_10_24_061309_create_ruangans_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,8 @@ return new class extends Migration
             $table->id();
             $table->string('nama_ruangan', 100); // Nama ruangan
             $table->integer('kapasitas')->default(0); // Kapasitas maksimal ruangan
-            $table->string('fasilitas', 255); 
+            $table->string('fasilitas', 255);
+            $table->enum('tipe', ['ruang_kuliah', 'ruang_diskusi', 'ruang_seminar']);
             $table->timestamps();
         });
     }
